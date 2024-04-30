a8c09694a7c34ff2fa5c175cd12759f12e5a07f7
mboulila <mahdi8boulila@gmail.com>
2024-04-30 12:28:27 -0400

    autoencoder training

--- main.py
+++ main.py
@@ -2,20 +2,25 @@ from src.midi_preprocess import get_midi_paths
 from src.midi_utils import get_data_from_midi, get_midi_from_data
 from net.autoencoder import Autoencoder, LossAccuracyCallback
 import numpy as np
+import tensorflow as tf
 
 data_path = 'data/data/lyricsMidisP0'
 # data_path = 'data/lyricsMidisP0'
 output_path = 'output/'
 
 if __name__ == "__main__":
+    #load midis
     midis = get_midi_paths(data_path, depth=2)
-    midis = midis[0][:10] # data is split into 5 sections, pick the first one.
+    n = 1
+    midis = midis[0][:n] # data is split into 5 sections, pick the first one.
     print('Total files: {}'.format(len(midis)))
-    
-    max_length = get_data_from_midi(midis[0]).shape[1]
+
+    midi_0 = get_data_from_midi(midis[0])
+    max_length = midi_0.shape[1]
+    max_pitches = midi_0.shape[0] #128 pitches
     for i in range(len(midis)):
         print('Processing file {}.'.format(i))
-        melody_lyrics_array = get_data_from_midi(midis[i], verbose=False)
+        melody_lyrics_array = get_data_from_midi(midis[i])
         curr_length = melody_lyrics_array.shape[1]
         if (max_length < curr_length):
             max_length = curr_length
@@ -23,14 +28,32 @@ if __name__ == "__main__":
         # output_filename = output_path + 'result' + str(i) +'.mid'
         # midi_reconstructed.write(output_filename)
 
-    data =[]
+    data = []
     for i in range(len(midis)):
-        mel_array = get_data_from_midi(midis[i], verbose=False)
+        mel_array = get_data_from_midi(midis[i])
         curr_length = mel_array.shape[1]
         if curr_length < max_length:
             padding = max_length - curr_length
             mel_array = np.pad(mel_array, ((0, 0), (0, padding)), mode='constant', constant_values=0)
-            data.append(mel_array)
+        data.append(mel_array)
 
-    data.append(melody_lyrics_array)
-    [print(x.shape) for x in data]
+    # [print(x.shape) for x in data]
+    x_train = tf.constant(data)
+    print("main.py, data shape:", x_train.shape)
+
+    model = Autoencoder(song_length= max_length,
+                        instrument_units= 1,
+                        pitch_units=max_pitches
+    )
+    model.compile(
+        optimizer = model.optimizer,
+        loss = model.loss
+    )
+    model.fit(x_train, 
+              x_train,
+              epochs = model.epochs,
+              batch_size = 2,
+            #   validation_split = 0.2,
+            #   callbacks = [LossAccuracyCallback()]
+    )
+
--- net/autoencoder.py
+++ net/autoencoder.py
@@ -1,44 +1,59 @@
 import tensorflow as tf
 
 class Autoencoder(tf.keras.Model):
-    def __init__(self, epochs = 100, instrument_units = 5, timestamp_fq = 1 / 100, pitch_units = 128):
+    def __init__(self, epochs = 10, instrument_units = 3, pitch_units = 128, song_length = 20000):
         super(Autoencoder, self).__init__()
         self.epochs = epochs
         self.instrument_units = instrument_units
-        self.timestamp_frequency = timestamp_fq
         self.pitch_units = pitch_units
-        self.song_length = (1 / self.timestamp_frequency ) * 60 * 60 * 5
+        self.song_length = song_length
 
         self.loss = tf.keras.losses.MeanSquaredError
-        self.optimizer = tf.keras.optimizers.Adam
+        self.optimizer = tf.keras.optimizers.Adam(1e-5)
+
         input_shape = (self.instrument_units, self.pitch_units, self.song_length)
-        self.encoder_layers = [
-            tf.keras.layers.Flatten(input_shape=input_shape),
-            tf.keras.layers.Dense(1028, activation='leakyRelu'),
-            tf.keras.layers.Dense(512, activation='leakyRelu'),
-            tf.keras.layers.Dense(256, activation='leakyRelu'),
-            tf.keras.layers.Dense(128, activation='leakyRelu')
-        ]
+        if (instrument_units == 1):
+            input_shape = (self.pitch_units, self.song_length)
+        flattened_dim = self.pitch_units * self.song_length
+        self.encoder = tf.keras.Sequential([
+            tf.keras.layers.Flatten(),
+            tf.keras.layers.Dense(flattened_dim // 2),
+            tf.keras.layers.LeakyReLU(),
+            tf.keras.layers.Dense(flattened_dim // 4),
+            tf.keras.layers.LeakyReLU(),
+            tf.keras.layers.Dense(flattened_dim // 8),
+            tf.keras.layers.LeakyReLU(),
+            tf.keras.layers.Dense(flattened_dim // 16),
+            tf.keras.layers.LeakyReLU(),
+        ])
         
-        self.decoder_layers = [
-            tf.keras.layers.Dense(128, activation='leakyRelu'),
-            tf.keras.layers.Dense(256, activation='leakyRelu'),
-            tf.keras.layers.Dense(512, activation='leakyRelu'),
-            tf.keras.layers.Dense(1028, activation='leakyRelu'),
+        self.decoder = tf.keras.Sequential([
+            tf.keras.layers.Dense(flattened_dim // 16),
+            tf.keras.layers.LeakyReLU(),
+            tf.keras.layers.Dense(flattened_dim // 8),
+            tf.keras.layers.LeakyReLU(),
+            tf.keras.layers.Dense(flattened_dim // 4),
+            tf.keras.layers.LeakyReLU(),
+            tf.keras.layers.Dense(flattened_dim // 2),
+            tf.keras.layers.LeakyReLU(),
+            tf.keras.layers.Dense(flattened_dim),
             tf.keras.layers.Reshape(input_shape)
-        ]
+        ])
 
     def call(self, x):
-        x = x
-        for layer in self.encoder_layers:
-            x = layer(x)
-        for layer in self.decoder_layers:
-            x = layer(x)
+        # for i, layer in enumerate(self.encoder.layers):
+        #     x = layer(x)
+            # print("encoder layer: ", layer, "output dim", x.shape)
+        # for i, layer in enumerate(self.decoder.layers):
+        #     x = layer(x)
+            # print("decoder layer: ", layer, "output dim", x.shape)
+        x = self.encoder(x)
+        x = self.decoder(x)
         return x
 
 class LossAccuracyCallback(tf.keras.callbacks.Callback):
     def on_epoch_end(self, epoch, logs=None):
-        if epoch % 10 == 0:
+        if epoch % 5 == 0:
             print("\nEpoch {} - Loss: {:.4f}".format(epoch, logs['loss']))
             
 if __name__ == "__main__":
